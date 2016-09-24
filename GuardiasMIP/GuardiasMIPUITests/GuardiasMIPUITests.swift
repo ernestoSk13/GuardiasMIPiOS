8dc79fcf3a29116659d05e09debcb102af43a15b
Ernesto Sánchez Kuri <ernesto.sanchez.kuri@gmail.com>
2016-09-23 22:13:28 -0500

    Latest changes

--- GuardiasMIP/GuardiasMIPUITests/GuardiasMIPUITests.swift
+++ GuardiasMIP/GuardiasMIPUITests/GuardiasMIPUITests.swift
@@ -29,6 +29,47 @@ class GuardiasMIPUITests: XCTestCase {
     }
     
     func testExample() {
+        
+        let app = XCUIApplication()
+        app.buttons["Crear Nuevo Rol"].tap()
+        
+        let nombreDeGuardiaEjATextField = app.textFields["Nombre de Guardia (ej: A)"]
+        nombreDeGuardiaEjATextField.tap()
+        app.textFields["Nombre de Guardia (ej: A)"]
+        
+        let button = app.otherElements.containingType(.NavigationBar, identifier:"Determina guardias").childrenMatchingType(.Other).element.childrenMatchingType(.Other).element.childrenMatchingType(.Other).element.childrenMatchingType(.Button).elementBoundByIndex(0)
+        button.tap()
+        nombreDeGuardiaEjATextField.tap()
+        app.textFields["Nombre de Guardia (ej: A)"]
+        button.tap()
+        app.typeText("c")
+        nombreDeGuardiaEjATextField.tap()
+        nombreDeGuardiaEjATextField.tap()
+        app.textFields["Nombre de Guardia (ej: A)"]
+        button.tap()
+        app.navigationBars["Determina guardias"].buttons["Siguiente"].tap()
+        
+        let element = app.otherElements.containingType(.NavigationBar, identifier:"Fechas").childrenMatchingType(.Other).element.childrenMatchingType(.Other).element.childrenMatchingType(.Other).element
+        element.childrenMatchingType(.Button).matchingIdentifier("Seleccionar Fecha").elementBoundByIndex(0).tap()
+        
+        let elegirButton = app.toolbars.buttons["Elegir"]
+        elegirButton.tap()
+        element.childrenMatchingType(.Button).matchingIdentifier("Seleccionar Fecha").elementBoundByIndex(1).tap()
+        app.datePickers.pickerWheels["2016"].tap()
+        elegirButton.tap()
+        app.navigationBars["Fechas"].buttons["Siguiente"].tap()
+        app.collectionViews.childrenMatchingType(.Cell).elementBoundByIndex(40).staticTexts["22"].tap()
+        app.childrenMatchingType(.Window).elementBoundByIndex(0).childrenMatchingType(.Other).element.childrenMatchingType(.Other).element.swipeUp()
+        
+        let guardiasNavigationBar = app.navigationBars["Guardias"]
+        guardiasNavigationBar.buttons["Hoy"].tap()
+        guardiasNavigationBar.buttons["?"].tap()
+        
+        let okButton = app.alerts["Calendario de Guardias"].collectionViews.buttons["Ok"]
+        okButton.tap()
+        okButton.tap()
+        guardiasNavigationBar.buttons["Cancelar"].tap()
+        
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
